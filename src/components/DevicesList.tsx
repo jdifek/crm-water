e748141c84318dd8fe1bed864ce1208689e28533
jdifek <givotkov11@gmail.com>
2025-03-20 13:08:30 +0200

    wqwq

--- src/components/DevicesList.tsx
+++ src/components/DevicesList.tsx
@@ -241,26 +241,26 @@
 
 // export default DevicesList
 
-import { Check, X } from 'lucide-react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { IPosDevice } from '../api/PosDevices/PosDevicesTypes'
-import { useDevice } from '../helpers/context/DeviceContext'
+import { Check, X } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IPosDevice } from "../api/PosDevices/PosDevicesTypes";
+import { useDevice } from "../helpers/context/DeviceContext";
 
-const StatusIcon = ({ status }: { status: 'true' | 'false' }) => {
-	const statusColors = {
-		true: 'text-green-500',
-		false: 'text-red-500',
-	}
+const StatusIcon = ({ status }: { status: "true" | "false" }) => {
+  const statusColors = {
+    true: "text-green-500",
+    false: "text-red-500",
+  };
 
-	const icons = {
-		true: Check,
-		false: X,
-	}
+  const icons = {
+    true: Check,
+    false: X,
+  };
 
-	const Icon = icons[status]
-	return <Icon className={statusColors[status]} />
-}
+  const Icon = icons[status];
+  return <Icon className={statusColors[status]} />;
+};
 
 // const StatusIcon = ({ status }: { status: 'ok' | 'error' | 'warning' }) => {
 // 	const statusColors = {
@@ -280,181 +280,183 @@ const StatusIcon = ({ status }: { status: 'true' | 'false' }) => {
 // }
 
 const DevicesList = () => {
-	const [searchQuery, setSearchQuery] = useState('')
-	const [isActiveFilter, setIsActiveFilter] = useState<boolean>(true)
-	const { devices, error, loading, fetchDevices, setSelectedDeviceId } =
-		useDevice()
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isActiveFilter, setIsActiveFilter] = useState<boolean>(true);
+  const { devices, error, loading, fetchDevices, setSelectedDeviceId } =
+    useDevice();
 
-	const filteredDevices = devices.filter(device =>
-		device.id.toString().includes(searchQuery)
-	)
+  const filteredDevices = devices.filter((device) =>
+    device.id.toString().includes(searchQuery)
+  );
 
-	const calculateTotal = (key: keyof IPosDevice) => {
-		return filteredDevices
-			.reduce((sum, device) => {
-				const value = device[key]
-				const numericValue = !isNaN(Number(value)) ? Number(value) : 0
-				return sum + numericValue
-			}, 0)
-			.toFixed(2)
-	}
+  const calculateTotal = (key: keyof IPosDevice) => {
+    return filteredDevices
+      .reduce((sum, device) => {
+        const value = device[key];
+        const numericValue = !isNaN(Number(value)) ? Number(value) : 0;
+        return sum + numericValue;
+      }, 0)
+      .toFixed(2);
+  };
 
-	const handleRefreshStats = () => {
-		fetchDevices()
-	}
+  const handleRefreshStats = () => {
+    fetchDevices();
+  };
 
-	const handleToggleActive = () => {
-		const newFilter = !isActiveFilter
-		setIsActiveFilter(newFilter)
-		fetchDevices(newFilter)
-	}
+  const handleToggleActive = () => {
+    const newFilter = !isActiveFilter;
+    setIsActiveFilter(newFilter);
+    fetchDevices(newFilter);
+  };
 
-	const handleDeviceClick = (id: number) => {
-		setSelectedDeviceId(id)
-	}
+  const handleDeviceClick = (id: number) => {
+    setSelectedDeviceId(id);
+  };
 
-	return (
-		<div className='p-4 lg:p-8'>
-			<div className='flex flex-col lg:flex-row justify-between items-center mb-6 space-y-4 lg:space-y-0'>
-				<h1 className='text-xl lg:text-2xl font-semibold'>Список аппаратов</h1>
-				<div className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto'>
-					<button
-						onClick={handleRefreshStats}
-						className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-sm lg:text-base'
-					>
-						Обновить статистику
-					</button>
-					<button
-						onClick={handleToggleActive}
-						className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-sm lg:text-base'
-					>
-						Деактивированные аппараты
-					</button>
-				</div>
-			</div>
+  return (
+    <div className="p-4 lg:p-8">
+      <div className="flex flex-col lg:flex-row justify-between items-center mb-6 space-y-4 lg:space-y-0">
+        <h1 className="text-xl lg:text-2xl font-semibold">Список аппаратов</h1>
+        <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
+          <button
+            onClick={handleRefreshStats}
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-sm lg:text-base"
+          >
+            Обновить статистику
+          </button>
+          <button
+            onClick={handleToggleActive}
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-sm lg:text-base"
+          >
+            {!isActiveFilter
+              ? "Aктивированные аппараты"
+              : "Деактивированные аппараты"}
+          </button>
+        </div>
+      </div>
 
-			<div className='bg-white rounded-lg shadow p-5 max-xl:max-w-2xl max-xl:mx-auto'>
-				<div className='flex items-center justify-between'>
-					<h1 className='text-xl lg:text-2xl font-semibold mb-5'>
-						Список аппаратов
-					</h1>
-					<div className='flex justify-between items-center mb-4'>
-						<span className='text-sm'>Поиск:</span>
-						<input
-							type='text'
-							placeholder='По ID'
-							value={searchQuery}
-							onChange={e => setSearchQuery(e.target.value)}
-							className='border-b border-gray-400 py-1 px-2 text-gray-700 focus:outline-none focus:border-blue-500 w-full sm:w-auto'
-						/>
-					</div>
-				</div>
-				{loading ? (
-					<div className='flex justify-center items-center py-6'>
-						<div className='animate-spin rounded-full h-8 w-8 border-b-2 border-gray-600'></div>
-					</div>
-				) : error ? (
-					<p className='text-center text-red-500 p-4'>{error}</p>
-				) : (
-					<div className='overflow-x-auto'>
-						<table className='min-w-full divide-y divide-gray-200 text-sm'>
-							<thead className='bg-gray-50'>
-								<tr>
-									{[
-										'ID',
-										'Аппарат',
-										'Адрес',
-										'Связь',
-										'Сенсор',
-										'Система',
-										'Объем резервуара (л)',
-										'Продано (л)',
-										'Заправка (л)',
-										'Остаток (л)',
-									].map((header, index) => (
-										<th
-											key={header}
-											className={`px-4 py-2  font-medium text-gray-500 tracking-wider ${
-												index === 0 || index === 1 || index === 2
-													? 'text-left'
-													: 'text-center'
-											} `}
-										>
-											{header}
-										</th>
-									))}
-								</tr>
-							</thead>
-							<tbody className='bg-white divide-y divide-gray-200'>
-								{filteredDevices.length > 0 ? (
-									filteredDevices.map(device => (
-										<tr key={device.id} className='hover:bg-gray-50'>
-											<td className='px-4 py-2'>
-												<Link
-													to={`/devices/details/${device.id}`}
-													onClick={() => handleDeviceClick(device.id)}
-													className='text-blue-500 hover:underline'
-												>
-													{device.id}
-												</Link>
-											</td>
-											<td className='px-4 py-2'>{device.name}</td>
-											<td className='px-4 py-2'>{device.address}</td>
-											<td className='px-4 py-2 text-center'>
-												<StatusIcon status={device.has_connection} />
-											</td>
-											<td className='px-4 py-2 text-center'>
-												<StatusIcon status={device.sensor_is_ok} />
-											</td>
-											<td className='px-4 py-2 text-center'>
-												<StatusIcon status={device.system_is_ok} />
-											</td>
-											<td className='px-4 py-2 text-center'>
-												{Number(device.tank_size)}
-											</td>
-											<td className='px-4 py-2 text-center'>
-												{Number(device.water_sold_since_last_refill)}
-											</td>
-											<td className='px-4 py-2 text-center'>
-												{Number(device.water_amount_after_last_refill)}
-											</td>
-											<td className='px-4 py-2 text-center'>
-												{device.water_left}
-											</td>
-										</tr>
-									))
-								) : (
-									<tr>
-										<td className='text-gray-700 font-semibold text-lg'>
-											Устройства не найдены
-										</td>
-									</tr>
-								)}
-							</tbody>
-							<tfoot className='bg-gray-100 font-bold text-center'>
-								<tr>
-									<td className='px-4 py-2'>Сумма</td>
-									<td className='px-4 py-2'></td>
-									<td className='px-4 py-2'></td>
-									<td className='px-4 py-2'></td>
-									<td className='px-4 py-2'></td>
-									<td className='px-4 py-2'></td>
-									<td className='px-4 py-2'>{calculateTotal('tank_size')}</td>
-									<td className='px-4 py-2'>
-										{calculateTotal('water_sold_since_last_refill')}
-									</td>
-									<td className='px-4 py-2'>
-										{calculateTotal('water_amount_after_last_refill')}
-									</td>
-									<td className='px-4 py-2'>{calculateTotal('water_left')}</td>
-								</tr>
-							</tfoot>
-						</table>
-					</div>
-				)}
-			</div>
-		</div>
-	)
-}
+      <div className="bg-white rounded-lg shadow p-5 max-xl:max-w-2xl max-xl:mx-auto">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl lg:text-2xl font-semibold mb-5">
+            Список аппаратов
+          </h1>
+          <div className="flex justify-between items-center mb-4">
+            <span className="text-sm">Поиск:</span>
+            <input
+              type="text"
+              placeholder="По ID"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="border-b border-gray-400 py-1 px-2 text-gray-700 focus:outline-none focus:border-blue-500 w-full sm:w-auto"
+            />
+          </div>
+        </div>
+        {loading ? (
+          <div className="flex justify-center items-center py-6">
+            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-600"></div>
+          </div>
+        ) : error ? (
+          <p className="text-center text-red-500 p-4">{error}</p>
+        ) : (
+          <div className="overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200 text-sm">
+              <thead className="bg-gray-50">
+                <tr>
+                  {[
+                    "ID",
+                    "Аппарат",
+                    "Адрес",
+                    "Связь",
+                    "Сенсор",
+                    "Система",
+                    "Объем резервуара (л)",
+                    "Продано (л)",
+                    "Заправка (л)",
+                    "Остаток (л)",
+                  ].map((header, index) => (
+                    <th
+                      key={header}
+                      className={`px-4 py-2  font-medium text-gray-500 tracking-wider ${
+                        index === 0 || index === 1 || index === 2
+                          ? "text-left"
+                          : "text-center"
+                      } `}
+                    >
+                      {header}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {filteredDevices.length > 0 ? (
+                  filteredDevices.map((device) => (
+                    <tr key={device.id} className="hover:bg-gray-50">
+                      <td className="px-4 py-2">
+                        <Link
+                          to={`/devices/details/${device.id}`}
+                          onClick={() => handleDeviceClick(device.id)}
+                          className="text-blue-500 hover:underline"
+                        >
+                          {device.id}
+                        </Link>
+                      </td>
+                      <td className="px-4 py-2">{device.name}</td>
+                      <td className="px-4 py-2">{device.address}</td>
+                      <td className="px-4 py-2 text-center">
+                        <StatusIcon status={device.has_connection} />
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        <StatusIcon status={device.sensor_is_ok} />
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        <StatusIcon status={device.system_is_ok} />
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        {Number(device.tank_size)}
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        {Number(device.water_sold_since_last_refill)}
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        {Number(device.water_amount_after_last_refill)}
+                      </td>
+                      <td className="px-4 py-2 text-center">
+                        {device.water_left}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td className="text-gray-700 font-semibold text-lg">
+                      Устройства не найдены
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+              <tfoot className="bg-gray-100 font-bold text-center">
+                <tr>
+                  <td className="px-4 py-2">Сумма</td>
+                  <td className="px-4 py-2"></td>
+                  <td className="px-4 py-2"></td>
+                  <td className="px-4 py-2"></td>
+                  <td className="px-4 py-2"></td>
+                  <td className="px-4 py-2"></td>
+                  <td className="px-4 py-2">{calculateTotal("tank_size")}</td>
+                  <td className="px-4 py-2">
+                    {calculateTotal("water_sold_since_last_refill")}
+                  </td>
+                  <td className="px-4 py-2">
+                    {calculateTotal("water_amount_after_last_refill")}
+                  </td>
+                  <td className="px-4 py-2">{calculateTotal("water_left")}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default DevicesList
+export default DevicesList;
